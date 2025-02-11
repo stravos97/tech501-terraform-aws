5b54f6b2af5d56ab2286a282eda83dfda0df66ba
Haashim <32544671+stravos97@users.noreply.github.com>
2025-02-11 15:50:20 +0000

    moved all data to new dir

--- create-ec2-instance/main.tf
+++ create-ec2-instance/main.tf
@@ -14,6 +14,16 @@
 provider "aws" {
   region = "eu-west-1" # Specify your desired AWS region
 }
+
+# # Create a VPC for the EC2 instance
+# resource "aws_vpc" "main" {
+#     cidr_block = "10.0.0.0/16"
+
+#     tags = {
+#         Name = "main-vpc"
+#     }
+# }
+
 resource "aws_instance" "app_instance" {
   # which AMI ID
   ami = "ami-0c1c30571d2dae5c9"
@@ -29,3 +39,23 @@ resource "aws_instance" "app_instance" {
     Name = "tech501-haashim-app"
   }
 }
+
+# # Create an EC2 instance
+# resource "aws_instance" "web_server" {
+#   # Amazon Linux 2 AMI (HVM) - Kernel 5.10
+#   ami           = "ami-0735c191cf914754d"
+#   instance_type = "t2.micro" # Free tier eligible instance type
+#   subnet_id     = aws_subnet.public.id
+
+#   # Add tags for better resource management
+#   tags = {
+#     Name        = "web-server"
+#     Environment = "development"
+#   }
+
+#   # EC2 instance volume configuration
+#   root_block_device {
+#     volume_size = 8     # Size in GB
+#     volume_type = "gp2" # General Purpose SSD
+#   }
+# }
